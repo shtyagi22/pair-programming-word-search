26f1594953472f5dfce720c1eac3b3837d1759bd
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-04 17:29:38 -0400

    wordserach function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  //console.log("horizontalJoin:", horizontalJoin)
+
+  const transpose = letters[0].map((col, i) => letters.map(row => row[i]));
+  const verticalJoin = transpose.map(ls => ls.join(''))
+  //console.log("verticalJoin:", verticalJoin)
+
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true
     }
+  }
+  for (let x of verticalJoin) {
+    if (x.includes(word)) {
+      return true
+    }
+  }
+
+  return false
 }
 
 module.exports = wordSearch
+
